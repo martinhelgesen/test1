3fcbc5d3ba9427c20a5318b6e6f77696b2a2546a
martinhelgesen <martin.helgesen@simployer.com>
2022-07-08 10:01:08 +0200

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace engagement.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
+
+    public string? AppContext { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+         _configuration = configuration;
+    }
+
+    public void OnGet()
+    {
+        AppContext = _configuration["AppContext"];
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,13 @@
+public class Startup 
+{
+    public IConfigurationRoot Configuration { get; set; }
+
+    public Startup(IWebHostEnvironment env)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        Configuration = builder.Build();
+    }
+}
